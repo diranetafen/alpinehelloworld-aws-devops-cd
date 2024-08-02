7d37522195774bea4f3222134cf0fcab3aefa241
Dirane TAFEN <diranetafen@yahoo.com>
2024-08-02 07:32:14 +0200

    Update deploy_release_info.sh

--- scripts/deploy_release_info.sh
+++ scripts/deploy_release_info.sh
@@ -5,7 +5,6 @@ PATH=$PATH:/usr/local/bin/
 yum install python3-pip -y
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
-pip3 install git-remote-codecommit
 rm -Rf alpinehelloworld || echo "folder already deleted"
 rm -Rf /root/deployment || echo "folder already deleted"
-git clone codecommit::us-east-1://alpinehelloworld
+aws s3 cp s3://alpinehelloworld-aws-devops-codebuild-ci/release/kustomization.yaml /root/deployment/
